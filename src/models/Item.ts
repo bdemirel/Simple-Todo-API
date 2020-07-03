70c66812c97416b91c7a2927f8a69c03fa40635c
Baris Demirel <bdemirel732@gmail.com>
2020-07-03 12:57:40 +0300

    Filled the models

--- src/models/Item.ts
+++ src/models/Item.ts
@@ -1,7 +1,17 @@
-import { Model } from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
 import sequelize from '../common/db';
+import List from './List';
 
 class Item extends Model {}
 Item.init({
-
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	name: DataTypes.STRING,
 }, { sequelize });
+
+Item.belongsTo(List);
+
+export default Item;
--- src/models/List.ts
+++ src/models/List.ts
@@ -1,7 +1,17 @@
-import { Model } from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
 import sequelize from '../common/db';
+import Item from './Item';
 
 class List extends Model { }
 List.init({
-
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	name: DataTypes.STRING,
 }, { sequelize });
+
+List.hasMany(Item);
+
+export default List;
